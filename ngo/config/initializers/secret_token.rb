3b8d9f7398824ee273a1dfbcd2cd5dd87bbb47fe
fotinik <fkaradimas@dius.com.au>
2013-06-01 11:17:36 +1000

    basic rails project for ngo

---
+++ ngo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ngo::Application.config.secret_token = '7cd9e8c1e0e9f40894febf3b8a82d56f232d6613014edd12a68d5f28ed7828dae0e071ddfcd9600912a823614bf4446649aeabb67be5bce6154694c376f28378'
